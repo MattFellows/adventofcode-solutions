acf326c9cd9cd47447c1ae32e62cc2cf6bc5aab0
Matt Fellows <github@matt-fellows.me.uk>
2024-12-14 14:52:13 +0000

    Day 14

--- 2024/day14ts/question14part1.ts
+++ 2024/day14ts/question14part1.ts
@@ -1,13 +1,15 @@
 import fs from 'fs'
-import { Location } from '../utils/grid'
+import { Grid, Location, makeGrid, printGrid } from '../utils/grid'
 
-const input = fs.readFileSync('./input-small.txt').toString()
+const input = fs.readFileSync('./input.txt').toString()
 const lines = input.split("\n").map(l => l.trim())
 
 interface Bot {
   p:Location
   v:Location
 }
+
+// console.log((-17%10))
 
 const makeBot = (line:string):Bot => {
   const parts = line.split(' ')
@@ -17,23 +19,40 @@ const makeBot = (line:string):Bot => {
   return { p: { x: Number(positionStr.split(',')[0]), y: Number(positionStr.split(',')[1]) }, v: { x: Number(velocitytr.split(',')[0]), y: Number(velocitytr.split(',')[1]) }}
 }
 
-const bots = lines.map(makeBot)
+const originalBots = lines.map(makeBot)
+const gridSize = {x:101,y:103}
+const gridStr = Array.apply(null, new Array(gridSize.y)).map(() => Array.apply(null, new Array(gridSize.x)).map(() => "0").join("")).join("\n")
+const emptyGrid = makeGrid<string>(gridStr)
+printGrid(emptyGrid)
+
 
 const iterateBot = (bot:Bot,iterations:number,gridSize:Location):Bot => {
   const newX = (bot.p.x + (iterations * bot.v.x)) % gridSize.x
   const newY = (bot.p.y + (iterations * bot.v.y)) % gridSize.y
-  return {...bot,p:{x:newX,y:newY}}
+  const X = newX < 0 ? gridSize.x - Math.abs(newX) : newX
+  const Y = newY < 0 ? gridSize.y - Math.abs(newY) : newY
+  const newBot = {...bot,p:{x:X,y:Y}}
+  // console.log(`Returning: ${newBot.p.x},${newBot.p.y}`)
+  return newBot
 }
 
-const gridSize = {x:11,y:7}
-const movedBots = bots.map(b => iterateBot(b,100,gridSize))
+const movedBots = originalBots.map(b => iterateBot(b,100,gridSize))
+
+const gridBots = (grid:Grid<string>,bots:Bot[]):Grid<string> => {
+  bots.forEach(b => {
+    // console.log(`Adding ${b.p.x},${b.p.y}`)
+    grid.rows[b.p.y].cells[b.p.x].val = `${Number(grid.rows[b.p.y].cells[b.p.x].val) + 1}`
+  })
+  return grid;
+}
 
 const countBotsInQuadrants = (bots:Bot[],gridSize:Location):number[] => {
   const values = [0,0,0,0]
   bots.forEach(b => {
     const p = b.p
-    const halfWidth = gridSize.x/2
-    const halfHeight = gridSize.y/2
+    const halfWidth = Math.floor(gridSize.x/2)
+    const halfHeight = Math.floor(gridSize.y/2)
+    // console.log(halfWidth,halfHeight)
     if (p.x < halfWidth && p.y < halfHeight) {
       values[0] +=1
     } else if (p.x < halfWidth && p.y > halfHeight) {
@@ -47,4 +66,8 @@ const countBotsInQuadrants = (bots:Bot[],gridSize:Location):number[] => {
   return values
 }
 
+const newGrid = gridBots(emptyGrid,movedBots)
+printGrid(newGrid)
+
 console.log(countBotsInQuadrants(movedBots,gridSize))
+console.log(countBotsInQuadrants(movedBots,gridSize).reduce((p,c) => p*c))
--- 2024/day14ts/question14part2.ts
+++ 2024/day14ts/question14part2.ts
@@ -1,4 +1,84 @@
 import fs from 'fs'
+import { Grid, Location, makeGrid, printGrid } from '../utils/grid'
 
-const input = fs.readFileSync('./input-small.txt').toString()
+const input = fs.readFileSync('./input.txt').toString()
 const lines = input.split("\n").map(l => l.trim())
+
+interface Bot {
+  p:Location
+  v:Location
+}
+
+// console.log((-17%10))
+
+const makeBot = (line:string):Bot => {
+  const parts = line.split(' ')
+  const positionStr = parts[0].substring(parts[0].indexOf('=')+1)
+  const velocitytr = parts[1].substring(parts[1].indexOf('=')+1)
+
+  return { p: { x: Number(positionStr.split(',')[0]), y: Number(positionStr.split(',')[1]) }, v: { x: Number(velocitytr.split(',')[0]), y: Number(velocitytr.split(',')[1]) }}
+}
+
+const originalBots = lines.map(makeBot)
+const gridSize = {x:101,y:103}
+const gridStr = Array.apply(null, new Array(gridSize.y)).map(() => Array.apply(null, new Array(gridSize.x)).map(() => "0").join("")).join("\n")
+const emptyGrid = makeGrid<string>(gridStr)
+printGrid(emptyGrid)
+
+
+const iterateBot = (bot:Bot,iterations:number,gridSize:Location):Bot => {
+  const newX = (bot.p.x + (iterations * bot.v.x)) % gridSize.x
+  const newY = (bot.p.y + (iterations * bot.v.y)) % gridSize.y
+  const X = newX < 0 ? gridSize.x - Math.abs(newX) : newX
+  const Y = newY < 0 ? gridSize.y - Math.abs(newY) : newY
+  const newBot = {...bot,p:{x:X,y:Y}}
+  // console.log(`Returning: ${newBot.p.x},${newBot.p.y}`)
+  return newBot
+}
+
+const gridBots = (grid:Grid<string>,bots:Bot[]):Grid<string> => {
+  const newGrid = makeGrid<string>(gridStr);
+    bots.forEach(b => {
+    // console.log(`Adding ${b.p.x},${b.p.y}`)
+    newGrid.rows[b.p.y].cells[b.p.x].val = `${Number(newGrid.rows[b.p.y].cells[b.p.x].val) + 1}`
+  })
+  return newGrid;
+}
+
+const contiguousHoriz = (bots:Bot[], x:number, y:number):number => {
+    let contiguousCount = 0;
+    for (let xx = x; xx < gridSize.x; xx++) {
+        if (bots.filter(b => b.p.x === xx && b.p.y === y).length) {
+            contiguousCount++
+        } else {
+            return contiguousCount
+        }
+    }
+    return contiguousCount
+}
+
+const calcContiguousHoriz = (bots:Bot[]):number => {
+    let total = 0
+    for (let y = 0; y < gridSize.y; y++) {
+        for (let x = 0; x < gridSize.x; x++) {
+            if (bots.filter(b => b.p.x === x && b.p.y === y).length) {
+                total += contiguousHoriz(bots, x, y)
+            }
+        }
+    }
+    return total
+}
+
+let arrangementEstimate = 0
+for (let i = 0; i < 100000; i++) {
+    const movedBots = originalBots.map(b => iterateBot(b,i,gridSize))
+
+    const maxContiguousHoriz = calcContiguousHoriz(movedBots)
+    if (maxContiguousHoriz > arrangementEstimate) {
+        arrangementEstimate = maxContiguousHoriz
+        const newGrid = gridBots(emptyGrid,movedBots)
+        console.log(`-----------------------------------${i} ${maxContiguousHoriz}-----------------------------------`)
+        printGrid(newGrid)
+        console.log(i)
+    }
+}
